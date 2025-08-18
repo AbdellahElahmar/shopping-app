cdeebc3a81c75f0cd45c8f2e22e521e7676c249f
AbdellahElahmar <elahmar.abdellah@gmail.com>
2025-08-18 15:31:43 +0100

    inventory service

---
+++ inventory-service/src/main/java/com/net/ElAhmar/inventory_service/controller/InventoryRestController.java
@@ -0,0 +1,20 @@
+package com.net.ElAhmar.inventory_service.controller;
+
+import com.net.ElAhmar.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/inventory")
+public class InventoryRestController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+}
---
+++ inventory-service/src/main/java/com/net/ElAhmar/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.net.ElAhmar.inventory_service.repository;
+
+import com.net.ElAhmar.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
